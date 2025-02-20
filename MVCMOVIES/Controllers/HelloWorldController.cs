3642fc1a4b3239c41b4f05ddc9be123b164111cc
lab_services_student <lab_services_student@labXGJZVK>
2025-02-20 12:30:12 +0200

    Add project files.

---
+++ MVCMOVIES/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MVCMovies.Controllers;
+
+
+public class HelloWorldController : Controller
+{
+    //
+    // GET: /HelloWorld/
+    public IActionResult index()
+    {
+        return View();
+    }
+
+    //
+    // GET: /HelloWorld/Welcome/
+    public string Welcome()
+    {
+        return "This is the Welcome action method...";
+    }
+}
+
